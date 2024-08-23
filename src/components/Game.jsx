2af3a2ff5854d806f292c9219cd7142f752776ee
Rubin Imeri <rubinimeri6@gmail.com>
2024-08-23 16:57:45 +0200

    Create gameHandler object & import components

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,13 +1,78 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import DifficultySelection from "./DifficultySelection";
 import Scoreboard from "./Scoreboard";
+import CardList from "./CardList";
 import { fetchImages } from "../utils";
 
 function Game() {
+    const [images, setImages] = useState(null);
+    const [selectedImageIds, setSelectedImageIds] = useState([]);
     const [difficulty, setDifficulty] = useState(null);
     const [score, setScore] = useState(0);
     const [highscore, setHighscore] = useState(0);
 
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await fetchImages(difficulty);
+            setImages(data);
+        }
+        fetchData();
+    }, [difficulty])
+
+    function shuffleImages() {
+        setImages(images => 
+            images
+            .map(value => ({ value, sort: Math.random() }))
+            .sort((a, b) => a.sort - b.sort)
+            .map(({ value }) => value)
+        )
+    }
+
+    function updateScore() {
+        const newScore = score + 1;
+
+        setScore(newScore)
+        if(newScore > highscore)
+            setHighscore(newScore);
+    }
+
+    const gameHandler = (() => {
+        function endGame() {
+            setDifficulty(null)
+            setImages(null);
+            setSelectedImageIds([]);
+            setScore(0);
+        }
+    
+        function continueGame(id) {
+            setSelectedImageIds([...selectedImageIds, id])
+            shuffleImages();
+            updateScore()
+        }
+
+        const win = images ? score === images.length : null;
+
+        return {
+            endGame,
+            continueGame,
+            win
+        }
+
+    })();
+
+    if(gameHandler.win) gameHandler.endGame()
+
+    function handleClick(e) {
+        const { id } = e.target.closest('div');
+
+        // Check if ID is in selectedImageIds
+        if(selectedImageIds.find(imageId => imageId === id))
+            gameHandler.endGame()
+
+        // Add ID to selectedImageIds && shuffle images
+        else 
+            gameHandler.continueGame()
+    }
     return (
         <main className="game">
             {difficulty === null ? 
@@ -17,6 +82,9 @@ function Game() {
                     <Scoreboard
                     score={score}
                     highscore={highscore} />
+                    <CardList
+                    images={images}
+                    handleClick={handleClick} />
                 </>) }
         </main>
     )
