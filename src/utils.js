5c41b7a18851951b11b5c1754971a97d1b329087
Rubin Imeri <rubinimeri6@gmail.com>
2024-08-25 15:10:38 +0200

    Change query to 'animal' instead of 'nature'

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,13 @@
 import { createClient } from "pexels";
 
 const API_KEY = 'DfYuKwhQAXH3ja73F6JioFLgLiHNKe7WXgON6jvvVrXK4yBRgbLlSF50';
-const query = 'Nature';
+const query = 'animal';
 const pexels = createClient(API_KEY);
 
 export const fetchImages = async (numberOfImages = 5) => {
     try {
         return await pexels.photos
-            .search({ query, page: 1, per_page: numberOfImages, orientation: 'portrait' })
+            .search({ query, page: 1, per_page: numberOfImages, orientation: 'landscape' })
             .then(images => images.photos);
     } catch(error) {
         console.error(error.message)
